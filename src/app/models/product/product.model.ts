feaafac2502f2475022044b8db46bd0bae67e61d
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2022-03-19 21:14:04 +0100

    add full product model

--- src/app/models/product/product.model.ts
+++ src/app/models/product/product.model.ts
@@ -1,4 +1,11 @@
 export interface ProductModel {
   id?: number;
   name?: string;
+  category?: string;
+  unit?: string;
+  packageSize?: number;
+  kcal?: number;
+  carbohydrates?: number;
+  protein?: number;
+  fat?: number;
 }
