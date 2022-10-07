bed9deee87aaf92ca105989c1a627b66be25c022
felixsteinke <steinke.felix@yahoo.de>
2022-10-07 13:39:36 +0200

    add service documentation

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -17,55 +17,91 @@ export class ProductService {
   constructor(private httpClient: HttpClient) {
   }
 
-  // === POST ========================================================================================================
-
-  public addProduct(productModel: ProductModel): Observable<ProductModel> {
-    const endpointUrl = this.controllerUrl + '/insert';
-    return this.httpClient.post<ProductModel>(endpointUrl, productModel, this.defaultHeaders);
-  }
-
-  // === GET =========================================================================================================
-
+  /**
+   * GET
+   * @return existing products
+   */
   public getAllProducts(): Observable<ProductModel[]> {
     return this.httpClient.get<ProductModel[]>(this.controllerUrl, this.defaultHeaders);
   }
 
+  /**
+   * GET
+   * @param productId database id
+   * @return matching object
+   */
   public getProductById(productId: number): Observable<ProductModel> {
     const endpointUrl = this.controllerUrl + '/get/' + productId;
     return this.httpClient.get<ProductModel>(endpointUrl, this.defaultHeaders);
   }
 
+  /**
+   * GET
+   * @param name partial name
+   * @return matching objects
+   */
   public searchProductsByName(name: string): Observable<ProductModel[]> {
     const endpointUrl = this.controllerUrl + '/search-name?name=' + name;
     return this.httpClient.get<ProductModel[]>(endpointUrl, this.defaultHeaders);
   }
 
+  /**
+   * GET
+   * @param category partial category
+   * @return matching objects
+   */
   public searchProductsByCategory(category: string): Observable<ProductModel[]> {
     const endpointUrl = this.controllerUrl + '/search-category?category=' + category;
     return this.httpClient.get<ProductModel[]>(endpointUrl, this.defaultHeaders);
   }
 
-  public getCategories(): Observable<string[]> {
-    const endpointUrl = this.controllerUrl + '/categories';
-    return this.httpClient.get<string[]>(endpointUrl, this.defaultHeaders);
+  /**
+   * POST
+   * @param productModel with input data
+   * @return created object
+   */
+  public addProduct(productModel: ProductModel): Observable<ProductModel> {
+    const endpointUrl = this.controllerUrl + '/insert';
+    return this.httpClient.post<ProductModel>(endpointUrl, productModel, this.defaultHeaders);
   }
 
-  public getUnits(): Observable<string[]> {
-    const endpointUrl = this.controllerUrl + '/units';
-    return this.httpClient.get<string[]>(endpointUrl, this.defaultHeaders);
-  }
-
-  // === PUT =========================================================================================================
-
+  /**
+   * PUT
+   * @param productModel with input data
+   * @return updated object
+   */
   public updateProduct(productModel: ProductModel): Observable<ProductModel> {
     const endpointUrl = this.controllerUrl + '/update';
     return this.httpClient.put<ProductModel>(endpointUrl, productModel, this.defaultHeaders);
   }
 
-  // === DELETE ======================================================================================================
-
+  /**
+   * DELETE
+   * @param productId database id
+   * @return deleted object
+   */
   public deleteProduct(productId: number): Observable<ProductModel> {
     const endpointUrl = this.controllerUrl + '/delete/' + productId;
     return this.httpClient.delete<ProductModel>(endpointUrl, this.defaultHeaders);
+  }
+
+  // === OPTION REQUESTS =============================================================================================
+
+  /**
+   * GET
+   * @return existing categories
+   */
+  public getCategories(): Observable<string[]> {
+    const endpointUrl = this.controllerUrl + '/categories';
+    return this.httpClient.get<string[]>(endpointUrl, this.defaultHeaders);
+  }
+
+  /**
+   * GET
+   * @return existing units
+   */
+  public getUnits(): Observable<string[]> {
+    const endpointUrl = this.controllerUrl + '/units';
+    return this.httpClient.get<string[]>(endpointUrl, this.defaultHeaders);
   }
 }
