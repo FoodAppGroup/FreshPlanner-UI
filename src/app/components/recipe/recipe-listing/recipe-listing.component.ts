ba1fe6b8c071ec294e403c5f88c9fabc77f1ab2e
felixsteinke <steinke.felix@yahoo.de>
2022-10-08 12:41:46 +0200

    restyle recipe listing to fit mobile view

--- src/app/components/recipe/recipe-listing/recipe-listing.component.ts
+++ src/app/components/recipe/recipe-listing/recipe-listing.component.ts
@@ -4,6 +4,8 @@ import {Location} from "@angular/common";
 import {RecipeService} from "../../../services/recipe.service";
 import {Router} from "@angular/router";
 import {RecipeModel} from "../../../models/recipe.model";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {OpenWarnSnackBar} from "../../../utility/snackbar";
 
 @Component({
   selector: 'app-recipe-listing',
@@ -12,28 +14,80 @@ import {RecipeModel} from "../../../models/recipe.model";
 })
 export class RecipeListingComponent implements OnInit {
 
-  recipeData: RecipeModel[] | undefined;
+  searchInput = '';
+  recipeList: RecipeModel[] = [];
+  categorizedRecipeMap: Map<string, RecipeModel[]> | undefined;
+  isLoading: boolean = false;
 
   constructor(private location: Location,
               private router: Router,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
-    this.loadRecipeSelection()
+    this.loadAllRecipes()
   }
 
-  public navigateToRecipeDetail(recipeId?: number): void {
+  public clickDisplayRecipe(recipeId: number | undefined): void {
+    if (!recipeId) {
+      OpenWarnSnackBar(this.snackBar, 'No recipe ID found.');
+      return;
+    }
     this.router.navigate(['/' + AppRoute.RECIPE_DISPLAY + '/' + recipeId]);
   }
 
-  private loadRecipeSelection(): void {
-    this.recipeService.getAllRecipes().subscribe((response) => {
-      this.recipeData = response;
+  public clickCreateNewRecipe(): void {
+    this.router.navigate([`/${AppRoute.RECIPE_EDITING}`]);
+  }
+
+  public clickSearchRecipes() {
+    const list = this.recipeList.filter(recipe => {
+      return recipe.name?.toLowerCase().includes(this.searchInput.toLowerCase());
+    });
+    this.categorizeRecipes(list);
+  }
+
+  private loadAllRecipes(): void {
+    this.isLoading = true;
+    this.recipeService.getAllRecipes().subscribe({
+      next: (response) => {
+        this.recipeList = response;
+        this.categorizeRecipes(response);
+        if (!this.categorizedRecipeMap) {
+          OpenWarnSnackBar(this.snackBar, 'No recipes found.');
+        }
+      },
+      error: (error) => {
+        console.log(error);
+        OpenWarnSnackBar(this.snackBar, 'Failed to load all recipes.');
+      }
+    }).add(() => {
+      this.isLoading = false;
     });
   }
 
-  public navigateBack(): void {
-    this.location.back();
+  private categorizeRecipes(recipes: RecipeModel[] | undefined): void {
+    if (!recipes || recipes.length === 0) {
+      this.categorizedRecipeMap = undefined;
+      return;
+    }
+    this.categorizedRecipeMap = new Map<string, RecipeModel[]>();
+    for (const recipe of recipes) {
+      const category = recipe.category;
+      if (category) {
+        this.addItemToMap(category, recipe);
+      } else {
+        this.addItemToMap('Others', recipe);
+      }
+    }
+  }
+
+  private addItemToMap(category: string, recipe: RecipeModel) {
+    if (this.categorizedRecipeMap?.has(category)) {
+      this.categorizedRecipeMap.get(category)?.push(recipe);
+    } else {
+      this.categorizedRecipeMap?.set(category, [recipe]);
+    }
   }
 }
